e7c746563e5d386bf781a2fd2c635cc1f8b413b0
Guilherme Santos <guilherme.santos@gmail.com>
2020-08-15 19:37:45 -0300

    answering more questions at chapter 4

--- chapter1.js
+++ chapter1.js
@@ -25,6 +25,10 @@ function main(arraySize) {
     timecheck(false)
 
     timecheck(true)
+    binarySearchRecursiveStart(randomNumberFromArray, sortedArray)
+    timecheck(false)
+
+    timecheck(true)
     linearSearch(randomNumberFromArray, sortedArray)
     timecheck(false)
 
@@ -48,12 +52,38 @@ function main(arraySize) {
             lowPosition = midPosition
         } else {
             console.log('----------------')
-            console.log('- binary search -')
+            console.log('- binary search -')            
             break;
         }
 
     }
 }
+
+function binarySearchRecursiveStart(target, numberArray) {
+    var lowPosition = 0
+    var topPosition = numberArray.length - 1
+
+    var position =  binarySearchRecursive(lowPosition, topPosition, numberArray, target)
+    console.log('----------------')
+    console.log('- recursive binary search -')
+    
+}
+
+function binarySearchRecursive(lowPosition, topPosition, numberArray, target) {    
+    sleep(1);        
+    var midBase = topPosition - lowPosition
+    var midPosition = ((midBase % 2) === 0 ? midBase / 2 : (midBase - 1) / 2) + lowPosition
+    var midValue = numberArray[midPosition]
+
+    if (target < midValue) {
+        binarySearchRecursive(lowPosition, midPosition, numberArray, target)            
+    } else if (target > midValue) {
+        binarySearchRecursive(midPosition, topPosition, numberArray, target)            
+    } else {        
+        return midPosition
+    }   
+}
+
 
  function linearSearch(target, numberArray) {
     for (var i = 0; i < numberArray.length; i++) {
---
+++ chapter4.js
@@ -0,0 +1,43 @@
+//exercises
+
+//4.1
+function sum(numberArray) {
+    return (numberArray.length === 0 ? 0 : (numberArray.pop(0) + sum(numberArray)))
+}
+
+
+//mainSum([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15])
+
+//4.2
+
+function counter(numberArray) {
+    return numberArray.pop() === undefined ? 0 : (1 + counter(numberArray))
+}
+
+
+//4.3
+
+function biggestNumber(numberArray, returnNumber) {
+    var numberIteration = numberArray.pop()
+    if (numberIteration === undefined) {        
+        return returnNumber
+    } else if (numberIteration > returnNumber) {        
+        return biggestNumber(numberArray, numberIteration)
+    } else {        
+        return biggestNumber(numberArray, returnNumber)
+    }
+}
+
+//4.4
+
+// check chapter1.js, method binarySearchRecursive
+
+
+    function main(numberArray) {
+        // console.log(sum(numberArray))
+        // console.log(counter(numberArray))
+        console.log(biggestNumber(numberArray, 0))
+    }
+
+
+    main([999999, 1, 2, 3, 10, 20, 2222, 30, 21])
