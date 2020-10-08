9be4081ec947b5e9961587e4f759b21d86034c0c
Guilherme Santos <guilherme.santos@gmail.com>
2020-10-07 23:10:37 -0300

    update chapter9 again

--- chapter9.js
+++ chapter9.js
@@ -3,27 +3,23 @@
 function selectPositionBestFit(equipments, limitWeight) {
         
     for (let index = equipments.length -1 ; index >= 0; index--) {
-        
-        //const element = array[index];
+                
         var  equipmentSelected
         var  positionEquipmentSelected
-        if (//(equipments[index].weight === limitWeight) || 
-         (equipments[index].weight <= limitWeight)) {
+        if (equipments[index].weight <= limitWeight) {
             
             if ((equipmentSelected === undefined) 
             || (equipmentSelected != undefined 
                 && (equipments[index].weight > equipmentSelected.weight))) {                
-                equipmentSelected = equipments[index]
-            //    console.log('index: ', index, ' - limitWeight: ', limitWeight, ' - equipmentSelected: ', equipmentSelected)
+                equipmentSelected = equipments[index]        
                 positionEquipmentSelected = index
             }
             
-     //       console.log('equipmentSelected: ', equipmentSelected)
-            //break
+     
         } 
         
     }
-    //console.log('---------------------- selected: ', equipmentSelected)
+    
     return positionEquipmentSelected
 }
 
@@ -37,15 +33,13 @@ function createEquipmentsArray() {
 
     equipments[2] = {
         name: "laptop",
-        // weight: 3,
-        weight: 3,
+        weight: 3,        
        price: 2000
     }
 
     equipments[1] = {
         name: "stereo",
-        // weight: 4,
-        weight: 3,
+        weight: 4,        
         price: 3000
     }
     return equipments 
@@ -55,14 +49,9 @@ function main ()  {
      
     var equipments = createEquipmentsArray()
     
-    
-     var returnMap = createMapForWeights(equipments)
+    var returnMap = createMapForWeights(equipments)
 
     printmap(returnMap)
-    
-
-    //console.log('1: ', selectPositionBestFit(equipments, 3))
-    //console.log('2: ', selectPositionBestFit(equipments, 5))
 }
 
 function createMapForWeights(equipments) {
@@ -71,16 +60,20 @@ function createMapForWeights(equipments) {
     let equipmentsLength = equipments.length 
     for (let index = 1; index <= equipmentsLength; index++) {        
                 
-        var positionBestFitForWeight = selectPositionBestFit(equipments, index)
+        var positionBestFitForWeight = selectPositionBestFit(equipments, index)        
         var bestFitForWeight = equipments[positionBestFitForWeight]        
+        console.log('index: ', index, ' - bestFit: ' , bestFitForWeight)
         
-        if (positionBestFitForWeight === 0) {                
-            equipments.shift()  
-        } else if (positionBestFitForWeight === equipments.length) {        
-            equipments.pop()
-        } else {       
-            equipments.splice(positionBestFitForWeight, 1)
-        }
+
+        //only add this block of code if you want to remove object after select it
+        
+        // if (positionBestFitForWeight === 0) {                
+        //     equipments.shift()  
+        // } else if (positionBestFitForWeight === equipments.length) {        
+        //     equipments.pop()
+        // } else {       
+        //     equipments.splice(positionBestFitForWeight, 1)
+        // }
         mapStuff.set(index, bestFitForWeight)       
     }
     return mapStuff
